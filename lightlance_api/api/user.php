3c8f157994a5b89b301fa601231f3d695129982f
chandrapramudya <candrapramudya5@gmail.com>
2022-01-09 22:12:45 +0700

    Lightlance_API

---
+++ lightlance_api/api/user.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'controller/user_controller.php';
+
+$url = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+$userController = UserController::getInstance();
+
+header('Content-Type: application/json');
+
+switch (end($url)) {
+    case 'topup':
+        $response = [
+            'status' => 'error',
+            'message' => 'your request is not valid',
+        ];
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $body = json_decode(file_get_contents('php://input'), true);
+
+            $id = $body['id'];
+            $nominal = $body['nominal'];
+
+            $response = [
+                'status' => 'success',
+                'topupNominal' => $nominal,
+                'balance' => $userController->topupBalance($id, $nominal),
+            ];
+        }
+
+        echo json_encode($response);
+
+        break;
+
+    default:
+        break;
+}
---
+++ lightlance_api/controller/user_controller.php
@@ -0,0 +1,58 @@
+<?php
+require_once 'utils/connection.php';
+
+class UserController extends Connection {
+    private static $instance = null;
+
+    protected function __construct()
+    {
+        
+    }
+
+    public static function getInstance() {
+        if (self::$instance == null) self::$instance = new UserController();
+
+        return self::$instance;
+    }
+
+    public function getUserById($id) {
+        $sql = 'SELECT * FROM user WHERE id_user = :id';
+        $stmt = $this->getConnection()->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        unset($user['password']);
+
+        return $user;
+    }
+
+    public function topupBalance($id, $topupNominal) {
+        $sql = 'UPDATE user SET saldo = saldo + :topupNominal WHERE id_user = :id';
+        $stmt = $this->getConnection()->prepare($sql);
+        $stmt->bindValue(':topupNominal', (int) $topupNominal);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+
+        return $this->getCurrentBalance($id);
+    }
+
+    public function getCurrentBalance($id) {
+        $sql = 'SELECT saldo FROM user WHERE id_user = :id';
+        $stmt = $this->getConnection()->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+
+        return (int)$stmt->fetch(PDO::FETCH_ASSOC)['saldo'];
+    }
+
+    public function updateBalance($id, $newBalance) {
+        $sql = 'UPDATE user SET saldo = :newBalance WHERE id_user = :id';
+        $stmt = $this->getConnection()->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->bindValue(':newBalance', $newBalance);
+        $stmt->execute();
+
+        return $stmt->rowCount() == 1;
+    }
+}
